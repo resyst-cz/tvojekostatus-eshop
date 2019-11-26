50aac9f56079fcaa11f87441f7c5744f51038c48
Peter Holop <peter.holop01@dixonscarphone.com>
2019-11-26 13:42:40 +0100

    added update script for products

--- _sql/changes.sql
+++ _sql/changes.sql
@@ -1,1 +1,8 @@
 -- --------- ALREADY ON STAGING ---------
+
+-- Wellbloud 2019-11-26 13:41
+UPDATE eko87stat_product_lang
+INNER JOIN w2018p_posts ON eko87stat_product_lang.id_product = w2018p_posts.id_new
+SET eko87stat_product_lang.description = w2018p_posts.description,
+eko87stat_product_lang.description_short = w2018p_posts.description_short
+
