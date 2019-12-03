0d32bdde969140ac465cf3f68082af5f7f8cfb65
Peter Holop <peter.holop01@dixonscarphone.com>
2019-12-03 10:15:13 +0100

    added SQL for finding image name

--- _sql/changes.sql
+++ _sql/changes.sql
@@ -1,10 +1,16 @@
+-- Wellbloud 2019-12-03 10:15
+-- find old images
+SELECT id_product, id_shop, id_lang, substr(description, locate("cms/old_wp", description)) as img
+FROM eko87stat_product_lang
+WHERE description LIKE '%cms/old_wp%';
+
+-- --------- ALREADY ON STAGING ---------
+
 -- Wellbloud 2019-12-03 09:28
 -- replaces path from old site in product description with new relative path
 UPDATE eko87stat_product_lang
 SET description = REPLACE(description, 'https://www.tvojekostatus.sk/wp-content/uploads/', 'cms/old_wp/')
 WHERE description LIKE '%https://www.tvojekostatus.sk/wp-content/uploads/%';
-
--- --------- ALREADY ON STAGING ---------
 
 -- Wellbloud 2019-11-26 13:41
 UPDATE eko87stat_product_lang
